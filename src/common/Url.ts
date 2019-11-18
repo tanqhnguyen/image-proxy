219997eaa3d53f4ac59cc24296e53b11b8542eac
Tan Nguyen <tan.qh.nguyen@gmail.com>
2019-11-18 18:23:59 +0200

    Add a new function to extract file extension

--- src/common/Url.ts
+++ src/common/Url.ts
@@ -1,4 +1,5 @@
 import { URL } from 'url';
+import * as mime from 'mime';
 
 export function normalizeUrl(url: string, query?: object): string {
   const urlObject = new URL(url);
@@ -7,4 +8,20 @@ export function normalizeUrl(url: string, query?: object): string {
   });
 
   return urlObject.toString();
+}
+
+export function extractFileExtension(
+  url: string,
+): { name: string; ext: string; mime: string } {
+  const urlObject = new URL(url);
+  const { pathname } = urlObject;
+  const parts = pathname.split('/');
+
+  const [fileName, fileExt] = parts.slice(-1)[0].split('.');
+
+  return {
+    name: fileName,
+    ext: fileExt,
+    mime: mime.getType(fileExt),
+  };
 }
