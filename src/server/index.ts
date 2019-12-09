e4920095b98d1cabf790492cb0149e26c1384b97
Tan Nguyen <tan.qh.nguyen@gmail.com>
2019-12-10 00:39:54 +0200

    Add a global prefix

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,7 +6,7 @@ export { Route, Controller } from './fastify';
 import { ImagesController } from '~controllers/Images';
 
 export function start(services: Services) {
-  const server = new FastifyServer();
+  const server = new FastifyServer({ prefix: '/api' });
 
   server.addController(new ImagesController({ services }), { prefix: '/v1' });
   server.listen(3000);
